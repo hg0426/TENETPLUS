c98b2b08d54514c13d95984dbdadcf5ea84a709a
김현규 / Kim Hyeonkyu <61915842+hg0426@users.noreply.github.com>
2024-02-21 14:16:31 +0900

    Update function_for_tenet.R

--- function_for_tenetplus/function_for_tenet.R
+++ function_for_tenetplus/function_for_tenet.R
@@ -584,3 +584,130 @@ function_PeakSource_Distance <- function(Tenet_result_dir=Tenet_result_dir,
                                   
 }
 
+pseudotime_heatmap2 <- function(matrix,selected_gene,gene_list,peak_list=F,ues_cell_select=F,cell_select=F,total_peak_gene,pseudotime, span=0.5,use_pseudotime_origin = T, use_z_score = T,
+                                order_pseudo_score=F,p_min=-0.7,p_max=0.7,p_legend=T,filename = "pseudotime_heatmap",
+                                max_min = T,out_result =F,target_average = F, target,pseudo_decrease=T,save_file=T){
+  #====================subset & order by pseudotime =================
+  sub_matrix <- matrix[,selected_gene]
+  sub_matrix <- cbind(pseudotime,sub_matrix)
+  sub_matrix <- subset(sub_matrix,sub_matrix[,1]!="Inf" & sub_matrix[,1]!="NA")
+  if(ues_cell_select!=F){
+    cell_select <- as.data.frame(cell_select)
+    sub_matrix <- sub_matrix[which(sample_cell_select==1),]
+  }
+  sub_matrix <- as.data.frame(sub_matrix)
+  
+  sub_matrix_order <- order(sub_matrix[,1])
+  
+  sub_matrix_sorted <- sub_matrix[sub_matrix_order,]
+  
+  sub_matrix_sorted <- as.matrix(sub_matrix_sorted)
+  
+  print(dim(sub_matrix_sorted))
+  
+  #====================use origin pseudotime or use order pseudotime =================
+  if(use_pseudotime_origin == T){
+  }else{
+    expanded_pseudotime <- data.frame(1:dim(sub_matrix_sorted)[1])
+    sub_matrix_sorted[,1] <- expanded_pseudotime[,1]
+  }
+  #=================== target average heatmap =================
+  if (target_average == T){
+    average_matrix <- sub_matrix_sorted[,1]
+    for (j in 1:length(selected_gene)){
+      if(target == "peak"){
+        temp_peak_list <- subset(peak_list,peak_list[,1]==selected_gene[j])
+        target_list <- gsub("-",".",unique(temp_peak_list[,3]))
+      }
+      if(target == "gene"){
+        temp_gene_list <- subset(gene_list,gene_list[,1]==selected_gene[j])
+        target_list <- gsub("-",".",unique(temp_gene_list[,3]))
+      }
+      target_list <- gsub("-",".",target_list)
+      temp_matrix <- as.data.frame(matrix[,target_list])
+      if (length(target_list)==1) {
+        colnames(temp_matrix) <- target_list
+        
+      }
+      temp_matrix <- cbind(pseudotime,temp_matrix)
+      temp_matrix <- subset(temp_matrix,temp_matrix[,1]!="Inf" & temp_matrix[,1]!="NA")
+      
+      temp_matrix <- as.data.frame(temp_matrix)
+      
+      temp_matrix_order <- order(temp_matrix[,1])
+      
+      temp_matrix_sorted <- temp_matrix[temp_matrix_order,]
+      
+      temp_matrix_sorted <- as.matrix(temp_matrix_sorted)
+      
+      if (length(target_list)!=1) {
+        temp_average <- apply(temp_matrix_sorted[,-1],1,mean)
+      }else{
+        temp_average <- temp_matrix_sorted[,2]
+      }
+      temp_average <- as.data.frame(temp_average)
+      colnames(temp_average) <- paste0(selected_gene[j])
+      average_matrix <- cbind(average_matrix,temp_average)
+    }
+    sub_matrix_sorted <- average_matrix
+  }
+  #====================z-score =================
+  
+  if(use_z_score == T){
+    for (i in 2:dim(sub_matrix_sorted)[2]){
+      #ave_merge_z[,i] <- log2(ave_merge_z[,i]+1)
+      sub_matrix_sorted[,i] <-(sub_matrix_sorted[,i]-mean(sub_matrix_sorted[,i]))/sd(sub_matrix_sorted[,i])
+    }
+  }
+  
+  #====================normalization =================
+  temp <- cbind(sub_matrix_sorted[,1], apply(sub_matrix_sorted[,-1], 2, function(j) {
+    k <- sub_matrix_sorted[,1]
+    lo <- loess(j~k, span = span)
+    xl <- seq(min(k),max(k), (max(k) - min(k))/(length(k)-1))
+    predict(lo,xl)
+  }))
+  colnames(temp)[1] <- "pseudotime"
+  temp <- t(temp)
+  
+  normalized_sub_matrix_sorted <- temp
+  #====================pseudo_score =================
+  if(order_pseudo_score==T){
+    pseudo_score <- data.frame()
+    
+    for (i in 2:dim(normalized_sub_matrix_sorted)[1]){
+      pseudo_score[i,1] <- sum(normalized_sub_matrix_sorted[1,]*normalized_sub_matrix_sorted[i,])
+    }
+    
+    normalized_sub_matrix_sorted_pseudo_ordered <- cbind(pseudo_score,normalized_sub_matrix_sorted)
+    rownames(normalized_sub_matrix_sorted_pseudo_ordered) <- rownames(normalized_sub_matrix_sorted)
+    temp <- normalized_sub_matrix_sorted_pseudo_ordered[-1,]
+    temp <- temp[order(temp[,1],decreasing = pseudo_decrease),]
+    normalized_sub_matrix_sorted_pseudo_ordered <- rbind(normalized_sub_matrix_sorted_pseudo_ordered[1,],temp)
+    final_matrix <- normalized_sub_matrix_sorted_pseudo_ordered
+    pheat_start <-2
+  }else{
+    final_matrix <- normalized_sub_matrix_sorted
+    pheat_start <-1
+  }
+  
+  #====================make pheatmap =================
+  if(max_min==T){
+    
+    s1 <- pheatmap(final_matrix[2:dim(final_matrix)[1],pheat_start:dim(final_matrix)[2]],
+                   breaks = seq(p_min,p_max,length.out=100),show_colnames=F, cluster_rows = F, cluster_cols = F,legend = p_legend)
+  }else{
+    s1 <- pheatmap(final_matrix[2:dim(final_matrix)[1],pheat_start:dim(final_matrix)[2]],
+                   show_colnames=F, cluster_rows = F, cluster_cols = F,legend = p_legend)
+    
+  }
+  if (save_file==T) {
+    png(paste0(filename,".png"),width=6000,height=4000,res=500)
+    print(s1)
+    dev.off()
+  }
+  
+  if (out_result==T){
+    return(final_matrix)
+  }
+}
